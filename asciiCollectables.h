d3c5c73b07a2b8a52e4b28765420cf75545656e4
FistingUranus <77898683+FistingUranus@users.noreply.github.com>
2022-01-21 17:04:50 -0500

    Blob small a,b and medium a,b

--- asciiCollectables.h
+++ asciiCollectables.h
@@ -3,3 +3,24 @@
 using namespace std;
 
 array<string, 2> blob = { R"(/\)", R"(\/)"};
+
+
+const string BlobASmall =
+    u8"\x1b[48;5;76m\x1b[38;5;15m/‾\\\x1b[m\n"
+    u8"\x1b[48;5;76m\x1b[38;5;15m\\_/\x1b[m\n";
+//cout << BlobASmall;
+
+const string BlobBSmall =
+    u8"\x1b[48;5;76m\x1b[38;5;15m/‾‾\\\x1b[m\n"
+    u8"\x1b[48;5;76m\x1b[38;5;15m\\__/\x1b[m\n";
+//cout << BlobBSmall;
+
+const string BlobAmedium =
+    u8"\x1b[48;5;76m\x1b[38;5;15m/‾Ō‾Ō\\\x1b[m\n"
+    u8"\x1b[48;5;76m\x1b[38;5;15m\\\x1b[4mO O \x1b[24m/\x1b[m\n";
+//cout << BlobAmedium;
+
+const string BlobBmedium =
+    u8"\x1b[48;5;76m\x1b[38;5;15m/Ō‾Ō‾\\\x1b[m\n"
+    u8"\x1b[48;5;76m\x1b[38;5;15m\\\x1b[4m O O\x1b[24m/\x1b[m\n";
+//cout << BlobBmedium;
