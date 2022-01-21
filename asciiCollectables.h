6e36a799807031b6769f48161be30bbac0dd4786
FistingUranus <77898683+FistingUranus@users.noreply.github.com>
2022-01-21 18:01:27 -0500

    Collectable into array instead of strings

--- asciiCollectables.h
+++ asciiCollectables.h
@@ -9,19 +9,24 @@ const array<string,2> BlobASmall = {
     u8"\x1b[48;5;76m\x1b[38;5;15m/‾\\\x1b[m\n",
     u8"\x1b[48;5;76m\x1b[38;5;15m\\_/\x1b[m\n"
 };
-//cout << BlobASmall;
+//for (int i = 0; i < BlobASmall.size(); ++i)
+    //cout << BlobASmall[i];
 
-const string BlobBSmall =
-    u8"\x1b[48;5;76m\x1b[38;5;15m/‾‾\\\x1b[m\n"
-    u8"\x1b[48;5;76m\x1b[38;5;15m\\__/\x1b[m\n";
-//cout << BlobBSmall;
+const array<string,2> BlobBSmall = {
+    u8"\x1b[48;5;76m\x1b[38;5;15m/‾‾\\\x1b[m\n",
+    u8"\x1b[48;5;76m\x1b[38;5;15m\\__/\x1b[m\n"};
+//for (int i = 0; i < BlobBSmall.size(); ++i)
+    //cout << BlobBSmall[i];
 
-const string BlobAmedium =
-    u8"\x1b[48;5;76m\x1b[38;5;15m/‾Ō‾Ō\\\x1b[m\n"
-    u8"\x1b[48;5;76m\x1b[38;5;15m\\\x1b[4mO O \x1b[24m/\x1b[m\n";
-//cout << BlobAmedium;
 
-const string BlobBmedium =
-    u8"\x1b[48;5;76m\x1b[38;5;15m/Ō‾Ō‾\\\x1b[m\n"
-    u8"\x1b[48;5;76m\x1b[38;5;15m\\\x1b[4m O O\x1b[24m/\x1b[m\n";
-//cout << BlobBmedium;
+const array<string,2> BlobAmedium = {
+    u8"\x1b[48;5;76m\x1b[38;5;15m/‾Ō‾Ō\\\x1b[m\n",
+    u8"\x1b[48;5;76m\x1b[38;5;15m\\\x1b[4mO O \x1b[24m/\x1b[m\n"};
+//for (int i = 0; i < BlobAmedium.size(); ++i)
+    //cout << BlobAmedium[i];
+
+const array<string,2> BlobBmedium = {
+    u8"\x1b[48;5;76m\x1b[38;5;15m/Ō‾Ō‾\\\x1b[m\n",
+    u8"\x1b[48;5;76m\x1b[38;5;15m\\\x1b[4m O O\x1b[24m/\x1b[m\n"};
+//for (int i = 0; i < BlobBmedium.size(); ++i)
+        //cout << BlobBmedium[i];
