c48ea03ea7b46977199c1cfe4b8b3eaa72ecf41a
FistingUranus <77898683+FistingUranus@users.noreply.github.com>
2022-01-21 18:23:16 -0500

    Monsters into array

--- asciiMonsters.h
+++ asciiMonsters.h
@@ -3,26 +3,30 @@
 using namespace std;
 
 //Monster A Form 1, small no spike
-	const string monsterAsmall = 
-		"\x1b[48;5;0m                \x1b[m\n"
-		"\x1b[48;5;0m    \x1b[48;5;9m  \x1b[48;5;11m    \x1b[48;5;9m  \x1b[48;5;0m    \x1b[m\n"
-		"\x1b[48;5;0m  \x1b[48;5;9m  \x1b[48;5;10m  \x1b[48;5;9m    \x1b[48;5;10m  \x1b[48;5;9m  \x1b[48;5;0m  \x1b[m\n"
-		"\x1b[48;5;0m  \x1b[48;5;11m  \x1b[48;5;9m  \x1b[48;5;10m    \x1b[48;5;9m  \x1b[48;5;11m  \x1b[48;5;0m  \x1b[m\n"
-		"\x1b[48;5;0m  \x1b[48;5;11m  \x1b[48;5;9m  \x1b[48;5;10m    \x1b[48;5;9m  \x1b[48;5;11m  \x1b[48;5;0m  \x1b[m\n"
-		"\x1b[48;5;0m  \x1b[48;5;9m  \x1b[48;5;10m  \x1b[48;5;9m    \x1b[48;5;10m  \x1b[48;5;9m  \x1b[48;5;0m  \x1b[m\n"
-		"\x1b[48;5;0m    \x1b[48;5;9m  \x1b[48;5;11m    \x1b[48;5;9m  \x1b[48;5;0m    \x1b[m\n"
-		"\x1b[48;5;0m                \x1b[m\n";
+	const array<string,8> monsterAsmall = {
+		"\x1b[48;5;0m                \x1b[m\n",
+		"\x1b[48;5;0m    \x1b[48;5;9m  \x1b[48;5;11m    \x1b[48;5;9m  \x1b[48;5;0m    \x1b[m\n",
+		"\x1b[48;5;0m  \x1b[48;5;9m  \x1b[48;5;10m  \x1b[48;5;9m    \x1b[48;5;10m  \x1b[48;5;9m  \x1b[48;5;0m  \x1b[m\n",
+		"\x1b[48;5;0m  \x1b[48;5;11m  \x1b[48;5;9m  \x1b[48;5;10m    \x1b[48;5;9m  \x1b[48;5;11m  \x1b[48;5;0m  \x1b[m\n",
+		"\x1b[48;5;0m  \x1b[48;5;11m  \x1b[48;5;9m  \x1b[48;5;10m    \x1b[48;5;9m  \x1b[48;5;11m  \x1b[48;5;0m  \x1b[m\n",
+		"\x1b[48;5;0m  \x1b[48;5;9m  \x1b[48;5;10m  \x1b[48;5;9m    \x1b[48;5;10m  \x1b[48;5;9m  \x1b[48;5;0m  \x1b[m\n",
+		"\x1b[48;5;0m    \x1b[48;5;9m  \x1b[48;5;11m    \x1b[48;5;9m  \x1b[48;5;0m    \x1b[m\n",
+		"\x1b[48;5;0m                \x1b[m\n" };
+	//for (int i = 0; i < monsterAsmall.size(); ++i)
+			//cout << monsterAsmall[i];
 	
 //Monster A form 2, small WITH spike
-	const string monsterAbig =
-		"\x1b[48;5;0m    \x1b[48;5;11m  \x1b[48;5;0m    \x1b[48;5;11m  \x1b[48;5;0m    \x1b[m\n"
-		"\x1b[48;5;0m  \x1b[48;5;11m  \x1b[48;5;9m  \x1b[48;5;11m    \x1b[48;5;9m  \x1b[48;5;11m  \x1b[48;5;0m  \x1b[m\n"
-		"\x1b[48;5;11m  \x1b[48;5;9m  \x1b[48;5;10m  \x1b[48;5;9m    \x1b[48;5;10m  \x1b[48;5;9m  \x1b[48;5;11m  \x1b[m\n"
-		"\x1b[48;5;0m  \x1b[48;5;11m  \x1b[48;5;9m  \x1b[48;5;10m    \x1b[48;5;9m  \x1b[48;5;11m  \x1b[48;5;0m  \x1b[m\n"
-		"\x1b[48;5;0m  \x1b[48;5;11m  \x1b[48;5;9m  \x1b[48;5;10m    \x1b[48;5;9m  \x1b[48;5;11m  \x1b[48;5;0m  \x1b[m\n"
-		"\x1b[48;5;11m  \x1b[48;5;9m  \x1b[48;5;10m  \x1b[48;5;9m    \x1b[48;5;10m  \x1b[48;5;9m  \x1b[48;5;11m  \x1b[m\n"
-		"\x1b[48;5;0m  \x1b[48;5;11m  \x1b[48;5;9m  \x1b[48;5;11m    \x1b[48;5;9m  \x1b[48;5;11m  \x1b[48;5;0m  \x1b[m\n"
-		"\x1b[48;5;0m    \x1b[48;5;11m  \x1b[48;5;0m    \x1b[48;5;11m  \x1b[48;5;0m    \x1b[m\n";
+	const array<string,8> monsterAbig = {
+		"\x1b[48;5;0m    \x1b[48;5;11m  \x1b[48;5;0m    \x1b[48;5;11m  \x1b[48;5;0m    \x1b[m\n",
+		"\x1b[48;5;0m  \x1b[48;5;11m  \x1b[48;5;9m  \x1b[48;5;11m    \x1b[48;5;9m  \x1b[48;5;11m  \x1b[48;5;0m  \x1b[m\n",
+		"\x1b[48;5;11m  \x1b[48;5;9m  \x1b[48;5;10m  \x1b[48;5;9m    \x1b[48;5;10m  \x1b[48;5;9m  \x1b[48;5;11m  \x1b[m\n",
+		"\x1b[48;5;0m  \x1b[48;5;11m  \x1b[48;5;9m  \x1b[48;5;10m    \x1b[48;5;9m  \x1b[48;5;11m  \x1b[48;5;0m  \x1b[m\n",
+		"\x1b[48;5;0m  \x1b[48;5;11m  \x1b[48;5;9m  \x1b[48;5;10m    \x1b[48;5;9m  \x1b[48;5;11m  \x1b[48;5;0m  \x1b[m\n",
+		"\x1b[48;5;11m  \x1b[48;5;9m  \x1b[48;5;10m  \x1b[48;5;9m    \x1b[48;5;10m  \x1b[48;5;9m  \x1b[48;5;11m  \x1b[m\n",
+		"\x1b[48;5;0m  \x1b[48;5;11m  \x1b[48;5;9m  \x1b[48;5;11m    \x1b[48;5;9m  \x1b[48;5;11m  \x1b[48;5;0m  \x1b[m\n",
+		"\x1b[48;5;0m    \x1b[48;5;11m  \x1b[48;5;0m    \x1b[48;5;11m  \x1b[48;5;0m    \x1b[m\n" };
+	//for (int i = 0; i < monsterAbig.size(); ++i)
+           //cout << monsterAbig[i];
 
 
 
