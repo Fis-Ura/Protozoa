c2a88d319ceab334d244c89f82749b14ec8bab9a
FistingUranus <77898683+FistingUranus@users.noreply.github.com>
2022-01-24 18:36:12 -0500

    Petit soucis de clarté dans le code

--- asciiLogo.h
+++ asciiLogo.h
@@ -3,7 +3,8 @@
 using namespace std;
 
 //Logo for end of intro
-const array<string, 16> logo = { R"(PPPPPPPPPPPPPPPPP   RRRRRRRRRRRRRRRRR        OOOOOOOOO     TTTTTTTTTTTTTTTTTTTTTTT     OOOOOOOOO     ZZZZZZZZZZZZZZZZZZZ     OOOOOOOOO                 AAA               )",
+const array<string, 16> logo = { 
+R"(PPPPPPPPPPPPPPPPP   RRRRRRRRRRRRRRRRR        OOOOOOOOO     TTTTTTTTTTTTTTTTTTTTTTT     OOOOOOOOO     ZZZZZZZZZZZZZZZZZZZ     OOOOOOOOO                 AAA               )",
 R"(P::::::::::::::::P  R::::::::::::::::R     OO:::::::::OO   T:::::::::::::::::::::T   OO:::::::::OO   Z:::::::::::::::::Z   OO:::::::::OO              A:::A              )",
 R"(P::::::PPPPPP:::::P R::::::RRRRRR:::::R  OO:::::::::::::OO T:::::::::::::::::::::T OO:::::::::::::OO Z:::::::::::::::::Z OO:::::::::::::OO           A:::::A             )",
 R"(PP:::::P     P:::::PRR:::::R     R:::::RO:::::::OOO:::::::OT:::::TT:::::::TT:::::TO:::::::OOO:::::::OZ:::ZZZZZZZZ:::::Z O:::::::OOO:::::::O         A:::::::A            )",
@@ -20,7 +21,8 @@ R"(P::::::::P          R::::::R     R:::::R OO:::::::::::::OO       T:::::::::T 
 R"(P::::::::P          R::::::R     R:::::R   OO:::::::::OO         T:::::::::T         OO:::::::::OO   Z:::::::::::::::::Z   OO:::::::::OO A:::::A                 A:::::A )",
 R"(PPPPPPPPPP          RRRRRRRR     RRRRRRR     OOOOOOOOO           TTTTTTTTTTT           OOOOOOOOO     ZZZZZZZZZZZZZZZZZZZ     OOOOOOOOO  AAAAAAA                   AAAAAAA)" };
 
-const array<string, 16> logo2 = { R"(PPPPPP PPPPPPPPPP   RRRRR RRRRR RRRRR        OOO OOOOO     TTTTTTTTT TTTTTTTT TTTT     OOOO OOOO     ZZZZZZ ZZZZZ ZZZZZZ     O OO OO O                 AAA               )",
+const array<string, 16> logo2 = { 
+R"(PPPPPP PPPPPPPPPP   RRRRR RRRRR RRRRR        OOO OOOOO     TTTTTTTTT TTTTTTTT TTTT     OOOO OOOO     ZZZZZZ ZZZZZ ZZZZZZ     O OO OO O                 AAA               )",
 R"(P: :::::: :::::: P  R:::: ::::::: :::R     OO:::::: ::OO   T ::::::::::: :::: :::T   OO::: :::::OO    : : :: :::::: :::Z    O:: ::::::OO              A ::               )",
 R"(P: :: :PPP PP:::: P R : :::RR RRR:::::R  O ::: ::::   ::OO T:: :  : :::: ::::: ::T OO  ::: :::::::OO   :::: :::  ::: ::Z OO:: : :::: :::OO           A :::               )",
 R"(PP:: ::P     P:::::PRR: : :R     R:::::RO:::: ::OOO:::::::OT::::: T:::::::T :::::TO::::  :OOO::  :::OZ  :  ZZ ZZZ :: :Z O:: ::::O O:::: ::O         A:  ::::A            )",
@@ -37,7 +39,8 @@ R"(P::::::::P          R:::: :R      :::::R OO: :::::::: ::OO       T: :::::::T 
 R"(P  :: ::            R::: ::R     R:::::    OO: :::::::OO         T:::::::::T         O :: ::::::OO   Z ::: ::::::::::::Z   O :::::: ::OO A ::::A                 A ::::A )",
 R"(PPPPP P PP           RRR RRR     RRR RRR     OOOOOOOOO           TTTTT  TTTT           O OOOO OO     ZZZZ ZZZ ZZ ZZZ ZZZ     O O  OOOO  A  A A                    A A   A)" };
 
-const array<string, 16> logo3 = { R"(P  PP P  P PP PP    RRR R RRR RRRRRRR        OOO OO OO     TTTT TT TTTT TTT TT TTT     OOO OOOO      ZZ  Z ZZZZ ZZZ Z ZZ     O OOO O O                 AAA               )",
+const array<string, 16> logo3 = { 
+R"(P  PP P  P PP PP    RRR R RRR RRRRRRR        OOO OO OO     TTTT TT TTTT TTT TT TTT     OOO OOOO      ZZ  Z ZZZZ ZZZ Z ZZ     O OOO O O                 AAA               )",
 R"(P: ::: :: : :: : P  R:::: :: : :  : :R     OO ::::: ::OO     :: :::  ::: : :: : :T   OO::  :: : O     : : :: :  : :  ::Z    O:: : :::  O              A ::               )",
 R"(P: ::  P    P:  : P R :  :: R RRR:  ::R  O ::: : ::   ::OO T:: :  :   :: ::::: ::T OO  : :  : ::::OO   :::  :::  ::: ::Z OO:: :  ::: :  O            A : :               )",
 R"(PP :  :P       :: :PRR: :  R     R::: : O:  :    OO: :::::OT: ::: T:::::::T ::::: O::::  :OOO::  :::OZ  :  ZZ ZZZ :: :Z O::  :::O    :: ::O         A:  ::::             )",
@@ -54,7 +57,8 @@ R"(P:: : :::P          R : : :       :: ::R OO: : :::::: ::OO        : :::::::T 
 R"(P     ::            R  : ::R     R:::::    OO: ::: :::OO         T :: : :: T         O :: : : : OO     ::  ::::: ::::::Z   O :::: : ::OO A : : A                 A :: :  )",
 R"(P P   P  P            RR RR      RR  RR      O OO OOOO           T T T  TT T           O O OO OO     ZZ Z Z Z ZZ Z Z Z Z     O O  OOO   A  A A                          A)" };
 
-const array<string, 16> logo4 = { R"(P  P  P  P P  PP    R R R RR  RR R RR        OO  OO OO     TT T TT T TT T T TT T T     O   OO O      Z   Z Z Z  Z Z Z ZZ     O O O O O                 AAA               )",
+const array<string, 16> logo4 = { 
+R"(P  P  P  P P  PP    R R R RR  RR R RR        OO  OO OO     TT T TT T TT T T TT T T     O   OO O      Z   Z Z Z  Z Z Z ZZ     O O O O O                 AAA               )",
 R"(P: : :  : :  : :    R : :  : : :  : :R         :: : : O      ::  ::   :  : :: : :T   O ::  :  : O     : : :: :  : :  ::      :: : : :  O              A :                )",
 R"( : ::  P    P:  :   R :  :: R   R:  ::R    ::: :  :   ::     : :  :   :  :::   ::T     : :  : : :  O   ::   ::   : : ::Z  O:: :  : : :                 :                 )",
 R"(PP :  :        :: :  R: :         : : : O:  :     O: : : :  : : :  :  :: :T :: ::  : ::  :  O::  : :O   :   Z  Z  :  :  O :  :::     :  : O         A:  :: :             )",
@@ -71,7 +75,8 @@ R"(P : : : :P          R : : :       ::  :R O   : : : :: ::O         : :: : ::  
 R"(P     :             R  : : R     R: :::      :  :: :: OO         T :: : :: T              : : : O      ::   : :: :: ::     O :: : : ::O  A : :                     :: :  )",
 R"(P     P                R  R      RR  R       O  O O                T T  T  T           O O O  O      Z  Z   Z  Z Z   Z Z       O   O    A    A                          A)" };
 
-const array<string, 16> logo5 = { R"(P     P  P     P    R   R R   R  R R         O    O  O      T T T  T  T T    T T       O    O            Z   Z  Z   Z Z      O O   O                   AAA               )",
+const array<string, 16> logo5 = { 
+R"(P     P  P     P    R   R R   R  R R         O    O  O      T T T  T  T T    T T       O    O            Z   Z  Z   Z Z      O O   O                   AAA               )",
 R"(P: : :  : :    :    R : :  :   :  : :          :  :   O               :  : :  : :    O :   :    O       :       : :   :      :    : :                 A                  )",
 R"( :  :           :     :     R       :        :    :    :     :    :   :  :       T     :    :   :            :   : : :        :  :   :                 :                 )",
 R"(P  :  :           :   : :         :   :  :  :     O    : :  :   :  :  :      :     :     :         :    :      Z  :  :  O    : :     :  : O         A   :  :             )",
@@ -88,7 +93,8 @@ R"(P :   :               :   :       :      O   :   : :  :           :  :    :  
 R"(      :             R      R      :   :      :   : :   O           :  :                     : :        :    : :  :   :       :  : : :    A : :                      : :  )",
 R"(P                      R             R          O                          T                  O         Z      Z       Z       O   O    A    A                          A)" };
 
-const array<string, 16> logo6 = { R"(P        P                R      R                   O      T                  T            O            Z            Z        O                       A                 )",
+const array<string, 16> logo6 = { 
+R"(P        P                R      R                   O      T                  T            O            Z            Z        O                       A                 )",
 R"(     :         :        :           :          :                           :  :      O :                              :      :    :                   A                  )",
 R"( :  :           :     :             :        :    :    :     :           :                                   :       :        :                                          )",
 R"(P  :  :                                                            :  :      :                          :                            :  : O             :                )",
