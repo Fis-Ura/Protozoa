d2a5f4c294aeabcccfb909bccbccfb633e853388
FistingUranus <77898683+FistingUranus@users.noreply.github.com>
2022-01-24 15:55:16 -0500

    Dying monster sprite a,b,c

--- asciiMonsters.h
+++ asciiMonsters.h
@@ -30,9 +30,45 @@ using namespace std;
 	//for (int i = 0; i < monsterAbig.size(); ++i)
            //cout << monsterAbig[i];
 
+//Monster A dying 
+    const array<string, 8> monsterAdyingA = {
+		"\x1b[48;5;234m\x1b[38;5;76m................\x1b[m\n"
+		"\x1b[48;5;234m\x1b[38;5;76m................\x1b[m\n"
+		"\x1b[48;5;234m\x1b[38;5;76m....\x1b[48;5;94m  \x1b[48;5;172m    \x1b[48;5;94m  \x1b[48;5;234m\x1b[38;5;76m....\x1b[m\n"
+		"\x1b[48;5;234m\x1b[38;5;76m....\x1b[48;5;208m  \x1b[48;5;76m    \x1b[48;5;208m  \x1b[48;5;234m\x1b[38;5;76m....\x1b[m\n"
+		"\x1b[48;5;234m\x1b[38;5;76m....\x1b[48;5;172m  \x1b[48;5;106m    \x1b[48;5;172m  \x1b[48;5;234m\x1b[38;5;76m....\x1b[m\n"
+		"\x1b[48;5;234m\x1b[38;5;76m....\x1b[48;5;1m  \x1b[48;5;214m    \x1b[48;5;1m  \x1b[48;5;234m\x1b[38;5;76m....\x1b[m\n"
+		"\x1b[48;5;234m\x1b[38;5;76m................\x1b[m\n"
+		"\x1b[48;5;234m\x1b[38;5;76m................\x1b[m\n" };
+    //for (int i = 0; i < monsterAdyingA.size(); ++i)
+        //cout << monsterAdyingA[i];
 
 
+    //Monster A dying 2
+    const array<string, 8> monsterAdyingB = {
+		"\x1b[48;5;234m\x1b[38;5;76m................\x1b[m\n"
+		"\x1b[48;5;234m\x1b[38;5;76m................\x1b[m\n"
+		"\x1b[48;5;234m\x1b[38;5;76m......\x1b[48;5;94m    \x1b[48;5;234m\x1b[38;5;76m......\x1b[m\n"
+		"\x1b[48;5;234m\x1b[38;5;76m....\x1b[48;5;94m  \x1b[48;5;100m    \x1b[48;5;94m  \x1b[48;5;234m\x1b[38;5;76m....\x1b[m\n"
+		"\x1b[48;5;234m\x1b[38;5;76m....\x1b[48;5;94m  \x1b[48;5;100m    \x1b[48;5;94m  \x1b[48;5;234m\x1b[38;5;76m....\x1b[m\n"
+		"\x1b[48;5;234m\x1b[38;5;76m......\x1b[48;5;94m    \x1b[48;5;234m\x1b[38;5;76m......\x1b[m\n"
+		"\x1b[48;5;234m\x1b[38;5;76m................\x1b[m\n"
+		"\x1b[48;5;234m\x1b[38;5;76m................\x1b[m\n" };
+    //for (int i = 0; i < monsterAdyingB.size(); ++i)
+        //cout << monsterAdyingB[i];
 
+    //Monster A dying 3
+    const array<string, 8> monsterAdyingC = {
+		"\x1b[48;5;234m\x1b[38;5;76m................\x1b[m\n"
+		"\x1b[48;5;234m\x1b[38;5;76m................\x1b[m\n"
+		"\x1b[48;5;234m\x1b[38;5;76m................\x1b[m\n"
+		"\x1b[48;5;234m\x1b[38;5;76m.......\x1b[48;5;76m  \x1b[48;5;234m\x1b[38;5;76m.......\x1b[m\n"
+		"\x1b[48;5;234m\x1b[38;5;76m................\x1b[m\n"
+		"\x1b[48;5;234m\x1b[38;5;76m................\x1b[m\n"
+		"\x1b[48;5;234m\x1b[38;5;76m................\x1b[m\n"
+		"\x1b[48;5;234m\x1b[38;5;76m................\x1b[m\n" };
+    //for (int i = 0; i < monsterAdyingC.size(); ++i)
+        //cout << monsterAdyingC[i];
 
 
 
